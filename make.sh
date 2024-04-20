194584638e9d5e12e7da73f4e16319e3ef2dfa7a
Wang Rongfei <xrowii@outlook.com>
2024-04-20 17:48:50 +0800

    chore: rename `lxl_init.c` to `plugin.c`

--- make.sh
+++ make.sh
@@ -3,10 +3,10 @@
 set -x
 
 cd lua-tree-sitter
-cp ../lxl_init.c src
+cp ../plugin.c src
 
 make "${@}" \
     'LUA_INC=-I../include -I../lite-xl/resources/include -I../pragtical/resources/include' \
     LUA_LDFLAGS= POSIX_LDFLAGS=
 
-rm src/lxl_init.c
+rm src/plugin.c
